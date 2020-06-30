2814fde2759e239e1aba026684be73b1aa6890af
Josias <josiastech@gmail.com>
2020-06-29 21:11:48 -0600

    feat(core): add category list

--- src/Giphy.js
+++ src/Giphy.js
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react'
 
-export const Giphy = () => (
-  <>
-    <h2>Giphy App</h2>
-    <hr />
-  </>
-);
+export const Giphy = () => {
+
+  const [categories, setCategories] = useState(['a', 'b', 'c'])
+  const handleAdd = () => {
+    setCategories( cats => [...cats, 'HunterXHunter'] );
+  }
+
+  return (
+    <>
+      <h2>Giphy App</h2>
+      <hr />
+
+      <button onClick={handleAdd}>Click </button>
+
+      <ol>
+        {
+          categories.map( category => {
+            return <li key={category}>{category}</li>
+          })
+        }
+      </ol>
+    </>
+  )
+}
